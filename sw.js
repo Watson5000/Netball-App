6c451489e8c4be2c89967ed9ac4859f8063f5184
Watson5000 <matt.watson@stoweaustralia.com.au>
2026-01-08 13:01:37 +1100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,54 +1,33 @@
-const CACHE_NAME = "netball-rotations-v1";
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = "benchboss-v1";
+
+const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
+  "./bb-netball.png",
+  "./apple-touch-icon.png",
+  "./favicon.png",
   "./sw.js"
 ];
 
-// Install: cache core files
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-// Activate: clean up old caches
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
     )
   );
   self.clients.claim();
 });
 
-// Fetch: serve from cache first, then network
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request)
-        .then((networkResponse) => {
-          // Cache new files as they are fetched
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
-        })
-        .catch(() => cachedResponse);
-    })
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
